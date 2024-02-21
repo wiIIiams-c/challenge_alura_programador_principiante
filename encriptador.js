d2cf9cf5c1a2d8dbdfac25374ae59624af33f3a2
Williams C <wilcontrerasf@gmail.com>
2024-02-21 14:43:23 -0300

    Test commit-push azure
    Se hace una prueba de actions para el commit push directo el cual deberia reconocer azure

--- encriptador.js
+++ encriptador.js
@@ -15,21 +15,31 @@ textoDeEntrada.addEventListener("input", function() {
     this.value = this.value.toLowerCase();
 });
 
+//funcion encargada de encriptar el texto
 function txtEncriptar(){
+    //variable que se le va asignar el nuevo texto encriptado
     let nuevoTexto = "";
+    //obtengo el texto que se va a encriptar
     let txtNoEncriptado = document.getElementById("txtEntrada").value;
 
+    //recorro letra por letra para poder encriptar
     for (let index = 0; index < txtNoEncriptado.length; index++) {
+        //asigno la letra de acuerdo a su indice
         let letra = txtNoEncriptado[index];
+        //asigno y concateno la letra al texto nuevo
+        //dentro del concat verifica si la letra es parte del arreglo para saber si encriptarla o no
         nuevoTexto = nuevoTexto.concat(letrasEncriptar.includes(letra)?traductorTexto(letra, "encriptar"):letra);
     }
     
+    //asigno de salida el texto encriptado
     document.getElementById("txtSalida").value = nuevoTexto;
 }
 
 function txtDesencriptar(){
-    let nuevoTexto = [];
     let txtEncriptado = document.getElementById("txtEntrada").value;
+    //array para el texto desencriptado
+    let nuevoTexto = [];
+    //array para separar el texto de entrada por palabras
     let textoPorPartes = [];
     //separar cada palabra y guardarlo en array para verificar
     textoPorPartes = txtEncriptado.split(" ");
@@ -45,12 +55,15 @@ function txtDesencriptar(){
             nuevaPalabra = nuevaPalabra.replace(elementSub[0], traductorTexto(elementSub[0], "desencriptar"))
         });
 
+        //agrego la palabra desencriptada a un nuevo array
         nuevoTexto.push(nuevaPalabra);
     });
     
+    //al texto de salida le asigno el texto desencriptado separado por espacios
     document.getElementById("txtSalida").value = nuevoTexto.join(" ");
 }
 
+//funcion encargada de encriptar o desencriptar el texto
 function traductorTexto(texto, accion){
     let textoTraducido = "";
     let letraEncriptada = {
